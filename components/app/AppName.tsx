4f8536762de247f336fbcd9d4bbe10d425ba1dc3
happyeric77 <happyeric77@gmail.com>
2023-02-11 23:19:54 +0900

    Next ts

--- components/app/AppName.tsx
+++ components/app/AppName.tsx
@@ -1,12 +1,10 @@
-import Web3Context from "../../src/Web3Context"
-import { useContext, useEffect } from "react"
+import Web3Context from "../../src/Web3Context";
+import { useContext, useEffect } from "react";
 
 export default function AppName(): JSX.Element {
-    const web3Context = useContext(Web3Context)
-    useEffect(()=>{
-        console.log(web3Context)
-    }, [web3Context.addr, web3Context.chainId])
-    return <>
-        DAPP boilerplate (Nextjs + typescript)
-    </>
+  const web3Context = useContext(Web3Context);
+  useEffect(() => {
+    console.log(web3Context);
+  }, [web3Context.addr, web3Context.chainId]);
+  return <>DAPP boilerplate (Nextjs + typescript)</>;
 }
